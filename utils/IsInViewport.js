90a3c280e4d91c190609c91bab042d53bba9b3db
Martin Szymanski <github@elfacht.com>
2019-07-04 10:14:24 +0200

    Fix trigger if element is larger than viewport

--- utils/IsInViewport.js
+++ utils/IsInViewport.js
@@ -1,16 +1,15 @@
 /**
  * Check if element is in viewport
  *
- * @param {Object} $el – DOM element to check
+ * @param {Object} $el - DOM element [required]
  * @return {Boolean}
- *
  */
 export default function($el) {
   var bounding = $el.getBoundingClientRect();
   return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+      bounding.bottom >= 0 &&
+      bounding.right >= 0 &&
+      bounding.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.left <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
