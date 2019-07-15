a7a63edfe371611832226505a0e99eda7e186da5
Martin Szymanski <github@elfacht.com>
2019-07-15 10:25:39 +0200

    Use ternary operator

--- utils/ClassAdd.js
+++ utils/ClassAdd.js
@@ -1,14 +1,10 @@
 /**
  * Add classes to an element
- * 
+ *
  * @param {Object} $element
  * @param {String} className
  * @return {Function}
  */
 export default function($element, className) {
-  if ($element.classList) {
-      $element.classList.add(className);
-  } else {
-      $element.className += ' ' + className;
-  }
+  $element.classList ? $element.classList.add(className) : $element.className += ' ' + className;
 }
